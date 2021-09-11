4f62cee0cf01da1fd47fdb6c6aa0d0154e50c17e
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-11 11:15:26 -0400

    Using the Image object properties to move the origin of the image to the center

--- main.lua
+++ main.lua
@@ -1,17 +1,11 @@
 function love.load()
     myImage = love.graphics.newImage("assets/images/sheep.png")
+    width = myImage:getWidth()
+    height = myImage:getHeight()
 end
 
 function love.draw()
-    -- Drawing an image in the canvas
-    love.graphics.draw(myImage, 100, 100)
-
-    -- Drawing an image 2x of its size
-    love.graphics.draw(myImage, 100, 100, 0, 2, 2)
-
-    -- Drawing a mirrored image
-    love.graphics.draw(myImage, 100, 100, 0, -1, 1)
-
-    -- Drawing an image and changin its origin point
-    love.graphics.draw(myImage, 100, 100, 0, 2, 2, 39, 50)
+    -- Using the Image object properties to move the origin of the image
+    -- to the center
+    -- love.graphics.draw(myImage, 100, 100, 0, 2, 2, width/2, height/2)
 end
