217b4acd412dc2c79669f22ea1f66b4603311cdd
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-11 11:22:42 -0400

    Color properties example

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 function love.load()
     myImage = love.graphics.newImage("assets/images/sheep.png")
-    width = myImage:getWidth()
-    height = myImage:getHeight()
+    -- Changin the background color to white
+    love.graphics.setBackgroundColor(1, 1, 1)
 end
 
 function love.draw()
-    -- Using the Image object properties to move the origin of the image
-    -- to the center
-    -- love.graphics.draw(myImage, 100, 100, 0, 2, 2, width/2, height/2)
+    -- Love does not use rgb system directly so we
+    -- have to divide rgb_number/255 so love can work with it
+    love.graphics.setColor(255/255, 200/255, 40/255, 127/255)
+    
+    -- this is the same as the line above
+    -- love.graphics.setColor(1, 0.78, 0.15, 0.5)
+    love.graphics.draw(myImage, 100, 100)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(myImage, 200, 100)
 end
