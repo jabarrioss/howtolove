26f0c97fa9940c99ccc053430fd70ad39d33352a
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-11 11:10:07 -0400

    Drawing an image in the canvas

--- main.lua
+++ main.lua
@@ -1,20 +1,8 @@
 function love.load()
-    Object = require "lib.rxi.classic"
-    -- Don't forget to load the file
-    require "shape"
-    require "rectangle"
-    require "circle"
-
-    r1 = Rectangle(50, 50, 100, 200)
-    r2 = Circle(50, 200, 100)
-end
-
-function love.update(dt)
-    r1:update(dt)
-    r2:update(dt)
+    myImage = love.graphics.newImage("assets/images/sheep.png")
 end
 
 function love.draw()
-    r1:draw()
-    r2:draw()
+    -- Drawing an image in the canvas
+    love.graphics.draw(myImage, 100, 100)
 end
