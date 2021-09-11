3e021f2d7889c8923dbc01f46aaef7878e18336f
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-11 11:12:16 -0400

    Full example, chapter 12

--- main.lua
+++ main.lua
@@ -3,6 +3,15 @@ function love.load()
 end
 
 function love.draw()
+    -- Drawing an image in the canvas
+    love.graphics.draw(myImage, 100, 100)
+
+    -- Drawing an image 2x of its size
+    love.graphics.draw(myImage, 100, 100, 0, 2, 2)
+
+    -- Drawing a mirrored image
+    love.graphics.draw(myImage, 100, 100, 0, -1, 1)
+
     -- Drawing an image and changin its origin point
     love.graphics.draw(myImage, 100, 100, 0, 2, 2, 39, 50)
 end
