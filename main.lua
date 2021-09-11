639c33f2e495e472201e04db750490199e4c3144
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-11 12:54:45 -0400

    I made the collission detection chapter and I added a little improvement to the code

--- main.lua
+++ main.lua
@@ -1,18 +1,57 @@
+-- Collission detection
+
 function love.load()
-    myImage = love.graphics.newImage("assets/images/sheep.png")
-    -- Changin the background color to white
-    love.graphics.setBackgroundColor(1, 1, 1)
+    -- Create 2 rectangles
+    Object = require("lib.rxi.classic")
+    require("shape")
+    require("rectangle")
+    r1 = Rectangle(10, 100, 100, 100)
+    r2 = Rectangle(250, 120, 150, 120)
+    
+end
+
+function love.update(dt)
+    r1:update(dt)
 end
 
 function love.draw()
-    -- Love does not use rgb system directly so we
-    -- have to divide rgb_number/255 so love can work with it
-    love.graphics.setColor(255/255, 200/255, 40/255, 127/255)
+    if checkCollission(r1, r2) then
+        r1.isColliding = true
+        r2.isColliding = true
+    else
+        r1.isColliding = false
+        r2.isColliding = false
+    end
     
-    -- this is the same as the line above
-    -- love.graphics.setColor(1, 0.78, 0.15, 0.5)
-    love.graphics.draw(myImage, 100, 100)
+    r1:draw()
+    r2:draw()
+end
 
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(myImage, 200, 100)
+function checkCollission(a, b)
+
+    -- Let's get both rectangles sides
+    local a_left = a.x
+    local a_right = a.x + a.width
+    local a_top = a.y
+    local a_bottom = a.y + a.height
+
+    local b_left = b.x
+    local b_right = b.x + b.width
+    local b_top = b.y
+    local b_bottom = b.y + b.height
+
+    --If Red's right side is further to the right than Blue's left side.
+    if  a_right > b_left
+    --and Red's left side is further to the left than Blue's right side.
+    and a_left < b_right
+    --and Red's bottom side is further to the bottom than Blue's top side.
+    and a_bottom > b_top
+    --and Red's top side is further to the top than Blue's bottom side then..
+    and a_top < b_bottom then
+        --There is collision!
+        return true
+    else
+        --If one of these statements is false, return false.
+        return false
+    end
 end
--- rectangle.lua
+++ rectangle.lua
@@ -6,8 +6,16 @@ function Rectangle:new(x, y, width, height)
     Rectangle.super.new(self, x, y)
     self.width = width
     self.height = height
-    
+    self.isColliding = false
 end
 function Rectangle:draw()
-    love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+    local mode
+
+    if self.isColliding then
+        mode = "fill"
+    else
+        mode = "line"
+    end
+    
+    love.graphics.rectangle(mode, self.x, self.y, self.width, self.height)
 end
